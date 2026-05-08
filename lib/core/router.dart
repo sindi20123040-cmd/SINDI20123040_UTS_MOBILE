8e2e9fc02f837839eba8517fe40114ad2b7652a1
Sindi Rahmadani <sindi20123040@digitechuniversity.ac.id>
2026-05-08 22:38:47 +0700

    feat: implement bottom navigation bar and main screen

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -2,12 +2,15 @@
 
 import 'package:go_router/go_router.dart';
 import '../presentation/pages/splash_page.dart';
-import '../presentation/pages/home_page.dart';
+import '../presentation/pages/main_screen.dart'; // Import MainScreen yang baru dibuat
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
+    // Halaman pertama kali aplikasi dibuka (Splash Screen)
     GoRoute(path: '/', builder: (context, state) => const SplashPage()),
-    GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+
+    // Halaman utama setelah Splash (Sekarang mengarah ke MainScreen)
+    GoRoute(path: '/home', builder: (context, state) => const MainScreen()),
   ],
 );
---
+++ lib/presentation/pages/main_screen.dart
@@ -0,0 +1,36 @@
+// lib/presentation/pages/main_screen.dart
+
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'crypto_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [const HomePage(), const CryptoPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.currency_bitcoin),
+            label: 'Crypto',
+          ),
+        ],
+      ),
+    );
+  }
+}
